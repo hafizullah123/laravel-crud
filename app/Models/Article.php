62ac2d8fe824ecb56cfba80eb5b91040a47a2fba
Hafizullah <hjihadwal@gmail.com>
2024-11-01 10:52:41 -0700

    complete  crud

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -1,10 +1,28 @@
-<?php
+<?php  
 
-namespace App\Models;
+namespace App\Models;  
 
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Model;  
 
-class Article extends Model
-{
-    //
+class Article extends Model  
+{  
+    // Specify the table if it's not the plural form of the model name  
+    // protected $table = 'articles';  
+
+    // Specify the fillable properties (mass assignable)  
+    protected $fillable = [  
+        'title',  // Add title column  
+        'content', // Add content column  
+    ];  
+
+    // Uncomment if you want to disable timestamps  
+    // public $timestamps = false;   
+
+    // Define any necessary relationships here  
+    /*  
+    public function comments()  
+    {  
+        return $this->hasMany(Comment::class);  
+    }  
+    */  
 }
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,7 @@
 <?php  
 
 use Illuminate\Support\Facades\Route;  
-
-Route::get('/', function () {  
-    return view('welcome');  
-});  
-
 use App\Http\Controllers\ArticleController;  
 
+// Route for the articles resource  
 Route::resource('articles', ArticleController::class);
